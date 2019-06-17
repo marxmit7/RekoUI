a2b38ad22818dfe773845e5bd643e50cc63de476
Amit <amkr.here@gmail.com>
2019-06-17 15:01:10 +0530

    v1 of reactUI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        id:"",
+        title: "",
+        created_on:"",
+        // image: null
+    };
+
+    handleChange = e => {
+        this.setState({
+            [e.target.id]: e.target.value
+        });
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+        console.log(this.state);
+        let form_data = new FormData();
+        form_data.append("title", this.state.title);
+        let url = "http://localhost:8000/api/test/";
+        axios
+            .post(url, form_data, {
+                headers: {
+                    "content-type": "multipart/form-data"
+                }
+            })
+            .then(res => {
+                console.log(res.data);
+            })
+            .catch(err => console.log(err));
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <form onSubmit={this.handleSubmit}>
+                    <p>
+                        <input
+                            type="text"
+                            placeholder="Title"
+                            id="title"
+                            value={this.state.title}
+                            onChange={this.handleChange}
+                            required
+                        />
+                    </p>
+
+                    <p>
+                        <input
+                            type="file"
+                            id="image"
+                            accept="image/png, image/jpeg"
+                            onChange={this.handleImageChange}
+                            required
+                        />
+                    </p>
+                    <input type="submit" />
+                </form>
+            </div>
+        );
+    }
 }
 
 export default App;
