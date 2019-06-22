120e6ad34b182b21456c57bf3f9712890d2f7f11
pymit <amkr.here@gmail.com>
2019-06-22 15:10:07 +0530

    printing id with fetch

--- src/main/Homepage/embed/index.js
+++ src/main/Homepage/embed/index.js
@@ -1,31 +1,46 @@
 import React, { Component } from "react";
 import "./index.css";
-import axios from "axios";
 import EmbedService from "./result";
 
-const  embedservice  =  new  EmbedService();
+const embedservice = new EmbedService();
 
 class EmbedNow extends Component {
-
     constructor(props) {
-		super(props)
+        super(props);
 
-		this.state = {
-			response:null
-		}
-	}
+        this.state = {
+			contents: [],
+			id_name:[],
+			file_url:[],
 
-	componentDidMount() {
-		var  self  =  this;
-		embedservice.getEmbeddingsList().then(function (result) {
-			self.setState({ response:  result.data})
-		});
-	}
+        };
+    }
+
+    componentDidMount() {
+        // var  self  =  this;
+        embedservice
+            .getEmbeddingsList()
+            .then(result => {
+                let contents = result.data.map(cloudData => {
+					// console.log((cloudData));
+					return (<div>{cloudData.id}</div>);
+                    // return <div > {JSON.stringify(cloudData, null, 2)}</div>;
+                });
+                this.setState({ contents: contents });
+                console.log("state", this.state.contents);
+            })
+            .catch(error => console.log(error));
+    }
 
     render() {
         return (
-            <div>
-				{JSON.stringify(this.state.response,null,2)}
+			<div>
+			{this.state.contents}
+
+
+                <div>
+                    <img width="100" />
+                </div>
             </div>
         );
     }
