47eee54e573036f310f9f65de6cf1e5494131fb7
pymit <amkr.here@gmail.com>
2019-06-23 14:03:02 +0530

    embeddings are now being added using UI

--- src/main/Homepage/embed/card.js
+++ src/main/Homepage/embed/card.js
@@ -3,9 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
   card: {
--- src/main/Homepage/embed/popdialog.js
+++ src/main/Homepage/embed/popdialog.js
@@ -1,56 +1,84 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import {AddNewContent} from "./buttons";
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import { AddNewContent } from "./buttons";
+import axios from "axios";
 
 export default function EmbdeFormDialog() {
-  const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
+    var file =null;
 
-  function handleClickOpen() {
-    setOpen(true);
-  }
+    function handleClickOpen() {
+        setOpen(true);
+    }
 
-  function handleClose() {
-    setOpen(false);
-  }
-  function handleCreate(){
+    function handleClose() {
+        setOpen(false);
+    }
+    function handleState(event)
+    {   console.log(event.target.files[0])
+        file = event.target.files[0]
+    }
+    function handleCreate(event) {
+        event.preventDefault();
+        let form_data = new FormData();
+        form_data.append('file',file);
+        let url = "http://localhost:8000/api/embednow/";
+        axios
+            .post(url, form_data, {
+                headers: {
+                    "content-type": "multipart/form-data"
+                }
+            })
+            .then(response => {
+                console.log(response);
+                })
+            .catch(console.log);
+    }
 
-  }
+    return (
+        <div>
+            <div onClick={handleClickOpen}>
+                <AddNewContent />
+            </div>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="form-dialog-title"
+            >
+                <DialogTitle id="form-dialog-title">Embed new face</DialogTitle>
+                <form onSubmit={handleState}>
+                <DialogContent>
+                    <DialogContentText>
+                        Keep the name of the image as ID name (e.g{" "}
+                        <b>Barack Obama.jpg</b> )
+                    </DialogContentText>
 
-  return (
-	<div>
-      <div onClick={handleClickOpen}>
-	  <AddNewContent/>
-      </div>
-      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">Embed new face</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Keep the name of the image as ID name (e.g <b>Barack Obama.jpg</b> )
-          </DialogContentText>
-		  <input
-		  accept="image/*"
-		  type="file"
-		//   style={{ display: "none" }}
-		//   onChange={this.handleState}
-		  id="contained-button-file"
-		  required
-	  />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="secondary">
-            Cancel
-		  </Button>
-          <Button onClick={handleCreate} color="primary">
-            Create
-		  </Button>
-        </DialogActions>
-      </Dialog>
-    </div>
-  );
+                    <input
+                        accept="image/*"
+                        type="file"
+                        //   style={{ display: "none" }}
+                          onChange={handleState}
+                        id="contained-button-file"
+                        required
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="secondary">
+                        Cancel
+                    </Button>
+                    <label htmlFor="contained-button-file">
+                        <Button onClick={handleCreate} color="primary">
+                            Create
+                        </Button>
+                    </label>
+                </DialogActions>
+                </form>
+            </Dialog>
+        </div>
+    );
 }
