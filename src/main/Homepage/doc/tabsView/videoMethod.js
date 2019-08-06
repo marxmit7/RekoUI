2516696ec31dd8f0a2897c0076854ed37b0fd999
pymit <amkr.here@gmail.com>
2019-08-06 15:29:18 +0530

    paper design

--- src/main/Homepage/doc/tabsView/videoMethod.js
+++ src/main/Homepage/doc/tabsView/videoMethod.js
@@ -1,16 +1,22 @@
 import React from "react";
 import VideoFR from "./VideoFR/VideoFR";
 import YouTubeProcessing from "./YouTube/YouTubeProcess";
-
+import Paper from "@material-ui/core/Paper";
 class VideoMethodTabs extends React.Component {
     render() {
         return (
             <div>
-            Video Porcessing
-                <VideoFR />
-                <br /> <br /> <br />
-            YouTube Video Processing
-                <YouTubeProcessing />
+                <Paper>
+                    <br /> Video Porcessing
+                    <VideoFR />
+                    <br />{" "}
+                </Paper>
+                <br />
+                <Paper>
+                    <br />
+                    YouTube Video Processing
+                    <YouTubeProcessing />
+                </Paper>
             </div>
         );
     }
