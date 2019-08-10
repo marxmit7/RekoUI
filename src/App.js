fe5095e6e8ec5e7dedfc7c9102b61ba3b28e5f86
pymit <amkr.here@gmail.com>
2019-08-10 23:26:14 +0530

    small fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from "react";
 import NavBar from "./header/index";
 import CoreBody from "./main/index";
-import ImageFR from "./main/Homepage/image_upload/index"
+import ImageFR from "./main/Homepage/image_upload/index";
 import EmbedNow from "./main/Homepage/embed/index";
 import VideoFR from "./main/Homepage/video_upload/index";
 import FeedBack from "./main/Homepage/feedback/index";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import APIsDOC from"./main/Homepage/doc/index";
-import {connect} from "react-redux";
+import APIsDOC from "./main/Homepage/doc/index";
+import { connect } from "react-redux";
 import * as actions from "./store/actions/auth";
 
-
 class App extends Component {
-    componentDidMount()
-    {
+    componentDidMount() {
         this.props.onTryAutoSignup();
     }
     render() {
@@ -21,34 +19,38 @@ class App extends Component {
             <div>
                 <div {...this.props}>
                     <BrowserRouter>
-                        <NavBar/>
+                        <NavBar />
                         <Switch>
                             <Route path="/" component={CoreBody} exact />
                             <Route path="/image" component={ImageFR} exact />
                             <Route path="/embed" component={EmbedNow} exact />
                             <Route path="/video" component={VideoFR} exact />
-                            <Route path="/feedback" component={FeedBack} exact />
-                            <Route path ="/doc" component ={APIsDOC} exact/>
+                            <Route
+                                path="/feedback"
+                                component={FeedBack}
+                                exact
+                            />
+                            <Route path="/doc" component={APIsDOC} exact />
                         </Switch>
                     </BrowserRouter>
-
                 </div>
             </div>
         );
     }
 }
 
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.token !== null
+    };
+};
 
-const mapStateToProps=state=>{
-    return{
-        isAuthenticated:state.token!==null,
-    }
-}
-
-const mapDispatchToProps=dispatch=>
-{
-    return{
-        onTryAutoSignup:()=>dispatch(actions.authCheckState())
-    }
-}
-export default connect(mapStateToProps,mapDispatchToProps)(App);
+const mapDispatchToProps = dispatch => {
+    return {
+        onTryAutoSignup: () => dispatch(actions.authCheckState())
+    };
+};
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(App);
