a04e27ba95b2f1220ae56b5fde6c4b76cf2a3585
pymit <amkr.here@gmail.com>
2019-08-10 14:52:03 +0530

    reducers redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.css";
+import thunk from "redux-thunk";
+import {Provider} from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, compose, applyMiddleware } from "redux";
+import reducer from "./store/reducers/auth";
+const composeEnhances = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhances(applyMiddleware(thunk)));
+
+const app ={
+	
+}
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/store/actions/actionTypes.js
@@ -0,0 +1,5 @@
+export const AUTH_START = "AUTH_START";
+export const AUTH_STOP = "AUTH_STOP";
+export const AUTH_SUCCESS = "AUTH_SUCCESS";
+export const AUTH_LOGOUT = "AUTH_LOGOUT";
+export const AUTH_FAIL = "AUTH_FAIL";
